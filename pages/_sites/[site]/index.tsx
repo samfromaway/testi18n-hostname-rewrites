36389529a5dd2b705d997061e2d08cc7ded268f2
Samuel_Roellin <samfromaway@gmail.com>
2022-03-28 14:36:45 +0200

    fix

--- pages/_sites/[site]/index.tsx
+++ pages/_sites/[site]/index.tsx
@@ -9,31 +9,10 @@ export default function Index({ locale, site }) {
   );
 }
 
-const mockDB = [
-  {
-    name: 'Site 1',
-    description: 'Subdomain + custom domain',
-    subdomain: 'subdomain-1',
-    customDomain: 'custom-domain-1.com',
-  },
-  {
-    name: 'Site 2',
-    description: 'Subdomain only',
-    subdomain: 'subdomain-2',
-    customDomain: null,
-  },
-  {
-    name: 'Site 3',
-    description: 'Subdomain only',
-    subdomain: 'subdomain-3',
-    customDomain: null,
-  },
-];
-
 export async function getStaticPaths() {
   return {
     paths: [],
-    fallback: true, // fallback true allows sites to be generated using ISR
+    fallback: 'blocking',
   };
 }
 
